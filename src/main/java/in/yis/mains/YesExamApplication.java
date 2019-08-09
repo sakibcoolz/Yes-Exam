0a8629a948ddd633870b16cd80c7ca6c6759fb5d
Sakib Mulla <mr.sakibmulla@gmail.com>
2019-08-10 02:08:34 +0530

    Initial commit

---
+++ src/main/java/in/yis/mains/YesExamApplication.java
@@ -0,0 +1,13 @@
+package in.yis.mains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesExamApplication.class, args);
+	}
+
+}
